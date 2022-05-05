7dce8019da18ce103b8c534287be81c751e9dc61
Grace <gracelidinskysmith@gmail.com>
2022-05-05 17:14:34 -0400

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get "/yarns", to: 'yarns#index'
   get "/yarns/:id", to: "articles#yarns"
+  resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
