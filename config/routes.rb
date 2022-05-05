926e0973270407fde07f42fc4b6006a14bef317c
Grace <gracelidinskysmith@gmail.com>
2022-05-05 15:15:48 -0400

    Add path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'yarns#index'
   get 'yarns/index'
+
+  get "/yarns", to: 'yarns#index'
+  get "/yarns/:id", to: "articles#yarns"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
